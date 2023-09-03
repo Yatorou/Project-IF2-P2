01c7308be3ae304ebd62b6daa2d6dca1cde7b545
Yatorou <aryaindsekul@gmail.com>
2023-09-03 15:17:50 +0700

    update after reset

--- public/script.js
+++ public/script.js
@@ -2,10 +2,20 @@ $(window).on("load", function () {
   $("#loading").hide();
 });
 
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 1) {
+
+var stickyNavTop = $("nav").offset().top;
+
+var stickyNav = function () {
+  var scrollTop = $(window).scrollTop(); 
+  if (scrollTop > stickyNavTop) {
     $("nav").addClass("sticky");
+    $("ul").addClass("stickys");
   } else {
     $("nav").removeClass("sticky");
+    $("ul").removeClass("stickys");
   }
+};
+
+$(window).scroll(function () {
+  stickyNav();
 });
