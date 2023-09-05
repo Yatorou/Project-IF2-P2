8ffffab8e04ea358bd1910c47d56ee8ce351091d
Yatorou <aryaindsekul@gmail.com>
2023-09-05 23:11:53 +0700

    new navbar

--- public/script.js
+++ public/script.js
@@ -2,20 +2,33 @@ $(window).on("load", function () {
   $("#loading").hide();
 });
 
-
+// Sticky
 var stickyNavTop = $("nav").offset().top;
 
 var stickyNav = function () {
   var scrollTop = $(window).scrollTop(); 
   if (scrollTop > stickyNavTop) {
     $("nav").addClass("sticky");
-    $("ul").addClass("stickys");
+    $(".main").addClass("tam");
   } else {
     $("nav").removeClass("sticky");
-    $("ul").removeClass("stickys");
+    $(".main").removeClass("tam");
   }
 };
 
 $(window).scroll(function () {
   stickyNav();
+});
+
+//Dropdown
+$(document).ready(function () {
+  $(document).on("click", function (e) {
+    const dropdown = $(".dropdown-menu");
+    const menuBtn = $("#menu-btn");
+
+    if (dropdown.length && menuBtn.length && !menuBtn.is(e.target)) {
+      dropdown.hide();
+      menuBtn.prop("checked", false);
+    }
+  });
 });
