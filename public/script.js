955c5f511c3811d619c6027dd88c72b5c2af7e0e
Yatorou <aryaindsekul@gmail.com>
2023-09-19 19:46:48 +0700

    before welcome banner change

--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
 $(window).on("load", function () {
   $("#loading").hide();
+  $('input[type="checkbox"]').each(function(){
+  	$(this).prop('checked', false);
+  });
 });
 
 // Sticky
 var stickyNavTop = $("nav").offset().top;
 
 var stickyNav = function () {
-  var scrollTop = $(window).scrollTop(); 
+  var scrollTop = $(window).scrollTop();
   if (scrollTop > stickyNavTop) {
     $("nav").addClass("sticky");
     $(".main").addClass("tam");
@@ -21,4 +24,38 @@ $(window).scroll(function () {
 });
 
 //Dropdown
+
+//Preview
+$(document).ready(function () {
+  var iname = $("#Name");
+  var oname = $("#bnavbars");
+  iname.on("keyup", function () {
+    var inamev = iname.val();
+    oname.text(inamev);
+  });
+
+  var imail = $("#Email");
+  var omail = $("#oemail");
+
+  imail.on("keyup", function () {
+    var imailv = imail.val();
+    omail.text(imailv);
+  });
+
+  var iaddress = $("#Address");
+  var oaddress = $("#oaddress");
+
+  iaddress.on("keyup", function () {
+    var iaddressv = iaddress.val();
+    oaddress.text(iaddressv);
+  });
+
+  $("#wbprvw").on("click", function () {
+    if ($(this).is(":checked")) {
+      $(".preview").removeClass("hide");
+    } else {
+      $(".preview").addClass("hide");
+    }
+  });
+});
 
